ac5ee5102923e7afcfbe7259d8060540ab0c1111
Andy Gayton <andy@thecablelounge.com>
2022-09-26 17:39:07 -0400

    more sketching

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,59 @@
-use std::collections::HashMap;
+use serde::Serialize;
 
 use markdown::Block::{Header, Paragraph, UnorderedList};
-use markdown::{ListItem, Span};
+use markdown::{Block, ListItem, Span};
 
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+enum Node {
+    Leaf(String),
+    Tree(String, Vec<Node>),
 }
 
 fn spans_to_markdown(spans: &Vec<Span>) -> String {
     return markdown::generate_markdown(vec![Paragraph(spans.to_vec())]);
 }
 
-fn m2j(s: &str) {
-    let tokens = markdown::tokenize(s);
-    // let j = serde_json::to_string(&tokens).unwrap();
-    let mut d = HashMap::new();
-
-    for token in tokens {
+fn sub_m2j(mut tokens: std::slice::Iter<Block>) -> Vec<Node> {
+    while let Some(token) = tokens.next() {
         match &token {
             Header(spans, _size) => {
                 let header = spans_to_markdown(&spans);
-                d.insert(header, "foo");
+                let children = sub_m2j(tokens);
+                return vec![Node::Tree(header, children)];
             }
 
             UnorderedList(items) => {
-                for item in items {
-                    match &item {
-                        ListItem::Simple(spans) => println!("{}", spans_to_markdown(&spans)),
-                        _ => todo!(),
-                    }
-                }
+                let items = items.iter().map(|item| match &item {
+                    ListItem::Simple(spans) => Node::Leaf(spans_to_markdown(&spans)),
+                    _ => todo!(),
+                });
+                return items.collect::<Vec<Node>>();
             }
 
             _ => todo!(),
         }
     }
-    println!("{}", serde_json::to_string(&d).unwrap());
+    return vec![];
+}
+
+fn m2j(s: &str) -> Vec<Node> {
+    let tokens = markdown::tokenize(s);
+    let tokens = tokens.iter();
+
+    return sub_m2j(tokens);
+
+    /*
+
+    // let j = serde_json::to_string(&tokens).unwrap();
+    let mut d = HashMap::new();
+
+
+    */
 }
 
 #[cfg(test)]
@@ -45,11 +63,12 @@ mod tests {
 
     #[test]
     fn test_m2j() {
-        m2j(indoc! {"
+        let j = m2j(indoc! {"
     # Todo
     - one
     - two
     - three"}
         .into());
+    println!("{}", serde_json::to_string(&j).unwrap());
     }
 }
